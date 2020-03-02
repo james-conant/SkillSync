ee42471d0a0ac1dd5dad96a18fb815403f24e109
Tay <taylor.mleigh@gmail.com>
2020-03-02 16:28:02 -0600

    front page init + icons

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,22 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+// import axios from "axios";
+import FrontPage from "./FrontPage.jsx";
 
 export default function App() {
-  useEffect(() => {
-    axios
-      .get("/test")
-      .then(function(response) {
-        console.log(response);
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
-  });
-  return <div>Hello World</div>;
+  // useEffect(() => {
+  //   axios
+  //     .get("/test")
+  //     .then(function(response) {
+  //       console.log(response);
+  //     })
+  //     .catch(function(error) {
+  //       console.log(error);
+  //     });
+  // });
+
+  return (
+      <div>
+        <FrontPage />
+      </div>
+    );
 }
