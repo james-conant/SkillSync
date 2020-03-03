cb1b26958848ac69a5c6d7e3be038f9c8020de2f
Tay <taylor.mleigh@gmail.com>
2020-03-02 18:14:02 -0600

    +CategorySquare component

---
+++ src/components/CategorySquare.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+export default function CategorySquare({name}) {
+
+  return (
+    <>
+      <img src={name} width="138px"/>
+    </>
+  );
+
+}
--- src/components/FrontPage.jsx
+++ src/components/FrontPage.jsx
@@ -1,11 +1,36 @@
 import React, { useState, useEffect } from "react";
-// import axios from "axios";
+import CategorySquare from "./CategorySquare.jsx";
 
 export default function FrontPage() {
 
   return (
     <div>
-      Front Page
+      <div className="searchBarContainer">
+        SEARCHBAR
+      </div>
+      <div className="logoContainer">
+        <img src="../../imgs/logos/SkillSync.png" height="100vw"/>
+      </div>
+      <div className="FrontPageContainer">
+        <div className="frontPageRow1">
+          <CategorySquare name={"../../imgs/categories/Music.png"} />
+          <CategorySquare name={"../../imgs/categories/Language.png"} />
+          <br /><CategorySquare name={"../../imgs/categories/Sports.png"} />
+          <CategorySquare name={"../../imgs/categories/Outdoors.png"} />
+          FEATURED MENTOR COMPONENT
+        </div>
+        <div className="homeQuoteContainer">
+          RANDOM QUOTE COMPONENT
+        </div>
+        <div className="categoryIconList">
+        <CategorySquare name={"../../imgs/categories/Crafts.png"} />
+        <CategorySquare name={"../../imgs/categories/VisualArt.png"} />
+        <CategorySquare name={"../../imgs/categories/MathScience.png"} />
+        <br /><CategorySquare name={"../../imgs/categories/Food.png"} />
+        <CategorySquare name={"../../imgs/categories/SocialSciences.png"} />
+        <CategorySquare name={"../../imgs/categories/Miscellaneous.png"} />
+        </div>
+      </div>
     </div>
   );
 
