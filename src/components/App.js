32ce4fdcaa9c107d76197ea44690a08a90044dff
Tay <taylor.mleigh@gmail.com>
2020-03-03 12:02:54 -0600

    login page skeleton

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,35 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import useForm from './CustomHooks';
+
 import FrontPage from "./FrontPage.jsx";
 import UserProfile from "./UserProfile.jsx";
 import LoginPage from "./LoginPage.jsx";
 
 export default function App() {
-  // useEffect(() => {
-  //   axios
-  //     .get("/test")
-  //     .then(function(response) {
-  //       console.log(response);
-  //     })
-  //     .catch(function(error) {
-  //       console.log(error);
-  //     });
-  // });
+  
+  const submit = () => {
+    // alert(`${inputs.worldName} has been saved`)
+    var info = inputs;
+    axios.post('http://localhost:4321/submit', info)
+    .then((res) => {
+      // console.log(res);
+      done();
+    })
+    .catch((err) => {
+      console.error('--> jeepers: ', err);
+    })
+  }
+  //initializes the custom form hook
+  const {inputs, handleInputChange, handleSubmit} = useForm(submit);
+
 
   return (
       <div>
-        <LoginPage />
+        <LoginPage 
+          inputs={inputs}
+          handleInputChange={handleInputChange}
+          handleSubmit={handleSubmit}/>
       </div>
     );
 }
